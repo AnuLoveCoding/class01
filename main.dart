d1326c5848f6d37ffbced73ad4ee2a646de2ae5c
AnuLoveCoding <anusrivas07@gmail.com>
2023-04-21 05:48:31 +0000

    'Dart language'

--- main.dart
+++ main.dart
@@ -1,8 +1,27 @@
 void main(List<String> args) {
-  var isalive = true;
-  print(isalive);
+  // var isalive = true;
+  // print(isalive);
 
-  var name = 'Anurag';
-  print(name);
+  // var name = 'Anurag';
+  // print(name);
+
+  // Data tyeps
+  // Numbers;
+  // String
+  // boolean;
+  // double;
+  // dynamiclly;
+
+  // var value = 99.478;
+  // print(value);
+
+
+  var salary = 30000;
+  if(salary > 45000){
+    print('Good\'s salary');
+  }else{
+    print('Bad\'s salary');
+  }
+  
 }
 
