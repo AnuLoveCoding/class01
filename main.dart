b724c4d88768a78bc27d5fee1ef5bcba066e999a
AnuLoveCoding <anusrivas07@gmail.com>
2023-04-30 12:21:38 +0000

    ''practise Dart"

--- main.dart
+++ main.dart
@@ -23,11 +23,90 @@ void main(List<String> args) {
   //   print('Bad\'s salary');
   // }
 
-  List student_frnd = ['Anurag','Manjit','suresh','pankaj','Manjit'];
+  // List student_frnd = ['Anurag','Manjit','suresh','pankaj','Manjit'];
 
-  for(var i in student_frnd){
-    print(i);
-  }
+  // for(var i in student_frnd){
+  //   print(i);
+  // }
+
+  // Switch case Statemets;
+  //  var  grade = 'B';
+
+  // switch(grade)  {
+
+  //   case 'A' : print('Best of Luck');
+  //     break;
+
+  //   case 'B' : print('Try some somemomets later');
+  //     break;
+
+  //   case 'C' : print("You are invalid");
+  //     break;
+
+  //   default: print('Thanks');
+  //     break;
+      
+  // }
+
+  // Map <String, int> name = {
+  //   'roll' : 895689,
+    
+  // }
+
+
+  
+  // Map use for dictionary to store key value pairs stores;
+  // var name = {
+  //   'username' : 'Anurag',
+  //   'Password' : 'Anurag123'
+  // };
+
+  // name['bool'] =  'true';
+
+  // print(name);
+
+
+  // for(var i =1 ; i <= 10; i++){
+  //   print('Anurag'+'\n');
+  // }
+
+  List name = ['Anurag','pineapple', 'apple' , 'lion', 'chinese'];
+
+  // for(var i = 0; i < name.length; i++){
+  //     print(name[i] + '\n');
+  // }
+
+
+  // var i = 0;
+
+  // while(i < name.length){
+  //   print(name[i]);
+
+  //   i++;
+  // }
+
+
+  // for(var i = 1; i <= 10; i++){
+
+  //   if(i == 4){
+  //     continue;
+  //   }
+    
+  //   print(i * 2);
+
+    
+    
+  // }
+
+  print(formula(5, 6));
+  
   
 }
+
+int formula(int a, int b){
+
+    int ans =  2 * (a + b);
+
+   return ans;
+  }
 
