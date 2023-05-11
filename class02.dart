f3c1e4fad68d8994662c07a220be0e62ed376cea
AnuLoveCoding <anusrivas07@gmail.com>
2023-05-11 16:32:22 +0000

    "Dart practise"

--- class02.dart
+++ class02.dart
@@ -5,11 +5,27 @@ var a = 6;
 
  // (a < 10) ? print(true) : print (false);
 
-  print(name(a, 6));
-  
+  // print(name(a, 6));
+
+  // student('Anurag', roll : 116007);
+
+  var map = [1,2,3,4,5,6,7,8];
+  map[2] = 66;
+
+  // print(map[2]);
+
+  for( var k in map){
+    print(k);
+  }
 }
 
-int name(int a, int b) =>  (a + b);
+// void student(var name, { required var roll, var age}){
+//   print("Name = $name");
+//   print('Roll = $roll');
+//   print('Age = $age');
+// }
+
+// int name(int a, int b) =>  (a + b);
   
 
 
